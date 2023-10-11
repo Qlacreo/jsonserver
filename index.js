5457b7b981e7edacc6554df49f5be1fb513c2a7e
Qlacreo <ale.oliva@duocuc.cl>
2023-10-11 14:45:06 -0300

    a

---
+++ index.js
@@ -0,0 +1,11 @@
+const JsonSerer = require("json-server");
+const server = Jsonserver.create();
+const router = JsonserS.router("bd.json")
+const middlewares = JsonServer.defaults();
+const port = process.env.PORT || 3001;
+
+
+
+server.use(middlewares);
+server.use(router);
+server.listen(port);
